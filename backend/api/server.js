c680ba59fdbc70de9900d57ddf3817074ad9a2f0
Hassnainabbasi <168479142+Hassnainabbasi@users.noreply.github.com>
2025-07-07 03:59:30 +0500

    Add

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,9 +1,93 @@
-import http from 'http'
-import app from '../app.js';
+// import express from "express";
+// import { db } from "./config/db.js";
+// import { favoritesTable } from "./db/schema.js";
+// import { and, eq } from "drizzle-orm";
+// import { ENV } from "./config/env.js";
+// import job from "./config/cron.js";
+// import cors from "cors";
 
-const port = 4000;
-const server = http.createServer(app);
+// const app = express();
+// const PORT = 5001;
 
-server.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
+// app.use(cors());
+
+// app.use(express.json());
+
+// app.get("/api/health", (req, res) => {
+//   res.status(200).json({ success: true });
+// });
+
+// job.start();
+
+// app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+//   try {
+//     const { userId, recipeId } = req.params;
+//     const result = await db
+//       .delete(favoritesTable)
+//       .where(
+//         and(
+//           eq(favoritesTable.userId, userId),
+//           eq(favoritesTable.recipeId, parseInt(recipeId))
+//         )
+//       );
+//     res.status(200).json({ message: "Recipe removed from favorites" });
+//   } catch (e) {
+//     console.log(e.message);
+//     res.status(500).json({ error: "Something went wrong" });
+//   }
+// });
+
+// app.get("/api/favorites/:userId", async (req, res) => {
+//   try {
+//     const { userId } = req.params;
+//     const userFavorites = await db
+//       .select()
+//       .from(favoritesTable)
+//       .where(eq(favoritesTable.userId, userId));
+//     res.status(200).json(userFavorites);
+//   } catch (e) {
+//     console.log(e.message);
+//     res.status(500).json({ error: "Something went wrong" });
+//   }
+// });
+
+// app.post("/api/favorites", async (req, res) => {
+//   try {
+//     const { userId, recipeId, title, image, servings, cookTime } = req.body;
+
+//     if (!userId || !recipeId || !title || !image || !servings || !cookTime) {
+//       return res.status(400).json({ message: "Missing required fields" });
+//     }
+
+//     const newFavorite = await db
+//       .insert(favoritesTable)
+//       .values({
+//         userId,
+//         recipeId,
+//         title,
+//         image,
+//         servings,
+//         cookTime,
+//       })
+//       .returning();
+
+//     res.status(201).json(newFavorite[0]);
+//   } catch (e) {
+//     if (e.message.includes("duplicate key") || e.message.includes("unique")) {
+//       return res
+//         .status(409)
+//         .json({ error: "Recipe already favorited by this user" });
+//     }
+//     console.log(e.message);
+//     res.status(500).json({ error: "Something went wrong" });
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on Port ${PORT}`);
+// });
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from Node.js API! in Server js" });
+}
+
--- mobile/constant/api.js
+++ mobile/constant/api.js
@@ -1,1 +1,2 @@
 export const ApiUrl = "http://192.168.0.110:5001/api";
+    
